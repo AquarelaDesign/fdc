3fa1be40a4c1708014a0879be145c18a7bc85979
Jose Augusto <aquarela.design@gmail.com>
2019-10-21 23:10:38 -0300

    Atualização de Layout

--- frontend/src/consts.js
+++
@@ -1,4 +0,0 @@
-export default {
-    API_URL: 'http://localhost:3003/v01',
-    OAPI_URL: 'http://localhost:3003/oapi',
-}
---
+++ frontend/src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Login from './pages/login'
+
+import Main from './pages/main'
+import ComponentBuilder from './pages/ComponentBuilder'
+import ExamplesWithJSX from './pages/ExamplesWithJSX'
+import Dashboard from './pages/rebuilds/Dashboard'
+import DashboardV2 from './pages/rebuilds/DashboardV2'
+import ChartJS from './pages/rebuilds/charts/ChartJS'
+import Widgets from './pages/rebuilds/Widgets'
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/main" component={Main} exact />
+        <Route path="/dashboard" component={Dashboard} exact />
+        <Route path="/dashboardv2" component={DashboardV2} exact />
+        <Route path="/component-builder" component={ComponentBuilder} />
+        <Route path="/examples-with-jsx" component={ExamplesWithJSX} />
+        <Route path="/chart-js" component={ChartJS} exact />
+        <Route path="/widgets" component={Widgets} exact />
+      </Switch>
+    </BrowserRouter>
+  )
+}
